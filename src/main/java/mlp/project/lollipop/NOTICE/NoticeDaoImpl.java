9c458ccf28aacd0cfc4a3db1f9050017a1c9e8dd
asadzxc <wany_winner@naver.com>
2022-03-18 18:04:57 +0900

    프로젝트 최종본

---
+++ src/main/java/mlp/project/lollipop/NOTICE/NoticeDaoImpl.java
@@ -0,0 +1,51 @@
+package mlp.project.lollipop.NOTICE;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("noticeDao")
+public class NoticeDaoImpl implements NoticeDao{
+	
+	@Autowired
+	SqlSessionTemplate sm;
+
+	@Override
+	public List<NoticeDto> getList(NoticeDto dto) {
+		// TODO Auto-generated method stub
+		return sm.selectList("Notice_getList", dto);
+	}
+
+	@Override
+	public int getTotal(NoticeDto dto) {
+		// TODO Auto-generated method stub
+		return sm.selectOne("Notice_getTotal", dto);
+	}
+
+	@Override
+	public NoticeDto getView(NoticeDto dto) {
+		// TODO Auto-generated method stub
+		return sm.selectOne("Notice_getView",dto);
+	}
+
+	@Override
+	public void insert(NoticeDto dto) {
+		sm.insert("Notice_insert", dto);
+		
+	}
+
+	@Override
+	public void update(NoticeDto dto) {
+		sm.update("Notice_update", dto);
+		
+	}
+
+	@Override
+	public void delte(NoticeDto dto) {
+		sm.delete("Notice_delete", dto);
+		
+	}
+
+}
